541759af6a9f8b62205e0ca9f91715f531ce80df
Matt Rhone <kritoke@gmail.com>
2017-11-05 17:10:46 -0600

    Work on displayCat function.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const $catContainerClass = $('.cat-container');
-const $clickCountClass = $('.click-counts');
+var cats = []; // array to store all the cats
 
 class Cat {
-    constructor(name, image, clicks) {
+    constructor(name, image) {
+        var $clickCountClass = $(`'.${this.name}-click-counts'`);
         this.name = name;
         this.image = image;
-        this.clicks = clicks;
+        this.clicks = 0;
     }
 
     incrementClick() {
@@ -13,14 +14,21 @@ class Cat {
     }
 
     displayCat() {
-
+        let catHTML = '';
+        let imgHTML = `<img src="${this.image}" alt="Click on Photo of ${this.name} to Increment Clicks">`;
+        let captionHTML = `<figcaption><p>Name: ${this.name}</p>
+        <p>Number of Clicks: <span class="${this.name}-click-counts">${this.clicks}</span></p>
+        </figcaption>`;
     }
 
     updateClicks() {
-        $clickCountClass.html(catClicks);
+        $clickCountClass.html(clicks);
     }
 }
 
+cats.push(new Cat('bob', 'img/catphoto1.jpg'));
+
 $catContainerClass.on('click', 'img', function() {
+
 
 });
