ff9e4bbf4594dc65a2cbfb9f554f25c1da51e6f5
Matt Rhone <kritoke@gmail.com>
2017-11-08 18:31:33 -0600

    Initial click name to display cat functionality

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ class Cat {
     }
 
     displayCat() {
-        var catHTML = '';
+        var catHTML = '<p>Click the cat: </p>';
         var imgHTML = `<img src="${this.image}" alt="Click on Photo of ${this.name} to Increment Clicks">`;
         var captionHTML = `<figcaption><p>Name: ${this.name}</p>
         <p>Number of Clicks: <span class="${this.name}-click-counts">${this.clicks}</span></p>
@@ -36,28 +36,29 @@ new Cat('phil', 'img/catphoto3.jpg');
 new Cat('mews', 'img/catphoto4.jpg');
 new Cat('eric', 'img/catphoto5.jpg');
 
-// cats.forEach(function(cat) {
-//     cat.displayCat();
-// });
-
-
 function catPickerDisplay() {
+    var catHTML = '<p>The following are the available cats, click to make one show up: </p>';
+    $catPickerClass.append(catHTML);
+
     cats.forEach(function(cat) {
         $catPickerClass.append(`<div class="${cat.name}">${cat.name}</div>`);
     });
+
 }
 
 catPickerDisplay();
 
+// check if a cat name is clicked on
 $catPickerClass.on('click', 'div', function() {
     var currClass = $(this).attr('class');
     cats.forEach(function(cat) {
         if (currClass === cat.name) {
-            console.log(cat.name + ' is found');
+            cat.displayCat();
         }
     })
 });
 
+// check if cat image is clicked on, increment counter if clicked
 $catContainerClass.on('click', 'img', function() {
     var currCat = this;
     cats.forEach(function(cat) {
